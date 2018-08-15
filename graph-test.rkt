fd983bb60d2d9742839e90f29339ce998c907ef2
Guilherme <guilhermehas@hotmail.com>
2018-08-15 13:44:26 -0300

    Add graphs files

---
+++ graph-test.rkt
@@ -0,0 +1,62 @@
+#lang racket
+
+(require rackunit)
+
+(require "graph.rkt")
+
+(check-equal? (dif (vector) (vector)) (vector) "dif of vector with no size")
+(check-equal? (dif (vector 1.) (vector 1.)) (vector 0.) "dif must be vector 0")
+(check-equal? (dif (vector 1. 2.) (vector 1. 1.2)) (vector 0. 0.8) "dif of size 2 not equal")
+
+(check-equal? (dist (vector 0. 3.) (vector 4. 0.)) 5.  "dif of pitagora theorem not equal")
+
+(check-equal? (enumerate-list (list) 0) (list) "enumerate empty list")
+(check-equal? (enumerate-list (list 2) 0) (list (list 2 0)) "enumerate list of element 2")
+(check-equal? (enumerate-list (list 2) 2) (list (list 2 2)) "enumerate list of element 2")
+
+(let* (
+    [law (vector 0)]
+    [ans1 (list (vector 1) 3)]
+    [ans2 (list (vector 2) 2)]
+
+    [ans1withD (list 3 1.)]
+    [ans2withD (list 2 2.)]
+
+    [vans (list (vector 1) (vector 2))])
+
+    (check-equal? (update-new-answer law (list 10 4.) ans2) ans2withD)
+    (check-equal? (update-new-answer law (list 3 1.) ans1) ans1withD)
+
+    (check-equal? (get-shortest-answer law (rest vans)) (list 0 2.))
+    (check-equal? (get-shortest-answer law vans) (list 0 1.))
+    (check-equal? (get-shortest-answer law (reverse vans)) (list 1 1.)))
+
+(let* (
+    [answers (list (vector 0))]
+    [d1 3]
+    [law1-ansi-dist (list 1 2 d1)]
+    [law2-law2i (list (vector 5) 11)])
+    (check-equal? (update-law answers law1-ansi-dist law2-law2i) law1-ansi-dist))
+
+(let* (
+    [answers (list (vector 0))]
+    [d1 7]
+    [law1-ansi-dist (list 1 2 d1)]
+    [law2-law2i (list (vector 5) 11)]
+    [law2-ansi-dist (list 11 0 5.)])
+    (check-equal? (update-law answers law1-ansi-dist law2-law2i) law2-ansi-dist))
+
+(let* (
+    [answers (list (vector 0.))]
+    [laws (list (vector 1.))]
+    [ansind-lawind-dist (list 0 0 1.)])
+    (check-equal? (get-shortest-law-answer laws answers) ansind-lawind-dist))
+
+(let* (
+    [answers (list (vector 0.) (vector 1.) )]
+    [laws (list (vector -3.) (vector 2.) )]
+    [ansind-lawind-dist (list 1 1 1.)])
+    (check-equal? (get-shortest-law-answer laws answers) ansind-lawind-dist))
+
+
+
---
+++ graph.rkt
@@ -0,0 +1,71 @@
+#lang racket
+
+ (require racket/trace)
+
+(provide 
+norm
+dif
+dist
+enumerate-list
+update-new-answer
+get-shortest-answer
+update-law
+get-shortest-law-answer
+)
+
+(define (norm vec)
+    (sqrt (for/fold ([sum 0.0])
+        ([x (in-vector vec)])
+        (+ sum (* x x)))))
+
+(define (dif v1 v2)
+    (vector-map - v1 v2))
+
+(define (dist v1 v2)
+    (norm (dif v1 v2)))
+
+(define (enumerate-list lista start)
+    (define (aux lista i)
+        (match lista
+            [(list) (list)]
+            [el (cons (list (first el) i) (aux (rest el) (add1 i)))]))
+    (aux lista start))
+
+(define (update-new-answer law last-min-dist-index answer-index)
+    (match (list last-min-dist-index answer-index)
+        [(list (list index dist-old) (list new-answer new-index))
+        (let* (
+            [dist-new (dist law new-answer)])
+            (if (< dist-new dist-old)
+                (list new-index dist-new)
+                (list index dist-old)))]))
+
+(define (get-shortest-answer law answers)
+    (match answers
+        [(list) (list -1 0.)]
+        [(list a) (list 0 (dist law a))]
+        [ans
+        (let*(
+            [f (first ans)]
+            [r (rest ans)]
+            [d0 (dist law f)])
+            (foldl (lambda (x y) (update-new-answer law y x)) (list 0 d0) (enumerate-list r 1)))]))
+
+(define (update-law answers old-lawi-ansi-dist new-law-lawi)
+    (match (list old-lawi-ansi-dist new-law-lawi)
+        [(list 
+            (list lawi ansi old-dist)
+            (list new-law new-lawi))
+                (let* (
+                    [new-ansi-dist (get-shortest-answer new-law answers)])
+                    (match new-ansi-dist
+                        [(list new-ansi new-dist)
+                            (if (< new-dist old-dist)
+                                (list new-lawi new-ansi new-dist)
+                                old-lawi-ansi-dist)]))]))
+
+(define (get-shortest-law-answer laws answers)
+    (define first-term (list -1 -1 +inf.f))
+    (define (up-law law term)
+        (update-law answers term law))
+    (foldl up-law first-term (enumerate-list laws 0)))
