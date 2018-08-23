1474b5ec39e8bd1fe251778e13a2931e1565b59d
Guilherme <guilhermehas@hotmail.com>
2018-08-23 10:59:33 -0300

    clean the code

--- src/tfidf/reading_xml.rkt
+++ src/tfidf/reading_xml.rkt
@@ -1,11 +1,16 @@
+#lang racket
+
 (require xml)
 (require txexpr)
 
 ; retorna o texto da primeira questão
 ; o "filter string?" é para retirar os caracteres inadequados, quando o XML original for refeito poderemos retirar esta parte
 
-(string-join (filter string? (rest (rest
-                (third (let* ((fp (open-input-file "2010-01.xml" #:mode 'text))
-                              (arq (xml->xexpr (document-element (read-xml fp))))
-                              (L (get-elements arq)))
-                         (first L)))))))
+(define fp (open-input-file "2010-01.xml" #:mode 'text))
+(define arq (xml->xexpr (document-element (read-xml fp))))
+(define L (get-elements arq))
+(define L-first-third-element (third (first L)))
+(define rest-L (rest (rest L-first-third-element)))
+(define just-L-string (filter string? rest-L))
+(define text-string (string-join just-L-string))
+text-string
