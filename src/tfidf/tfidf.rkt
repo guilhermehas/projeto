2cbe47fdb68ba7ba791a7c2b4796ac655db8ab8c
joaoc <joao.carabetta@gmail.com>
2018-08-30 09:52:35 -0300

    tf for any sentence

--- src/tfidf/tfidf.rkt
+++ src/tfidf/tfidf.rkt
@@ -1,1 +1,15 @@
 #lang racket
+
+(define (treat-strings string)
+    (string-downcase (string-normalize-spaces string)))
+
+;; string --> (list of (list of 'str' float))
+(define (tf string)
+    (let* ((list-string (string-split (treat-strings string)))
+            (total (length list-string)))
+        (map (lambda (x) (list (first x) (/ (second x) total)))
+            (map (lambda (x) (list (first x) (length x)))
+            (group-by (lambda (x) x) list-string))))
+)
+
+(tf "guilherme foi Para a praia e foi para a fazenda")
