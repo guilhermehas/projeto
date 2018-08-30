f7ab6d98d7e5a30072a488196c4aeaa2fe77fa61
joaoc <joao.carabetta@gmail.com>
2018-08-30 09:27:51 -0300

    deleted unused module reading xml

--- src/tfidf/reading_xml.rkt
+++ src/tfidf/reading_xml.rkt
@@ -8,16 +8,6 @@
  xml/path
  )
 
-; retorna o texto da primeira questão
-; o "filter string?" é para retirar os caracteres inadequados, quando o XML original for refeito poderemos retirar esta parte
-
-(define (get-sentence)
-    (string-join (filter string? (rest (rest
-                (third (let* ((fp (open-input-file "data/raw/provas/2010-01.xml" #:mode 'text))
-                              (arq (xml->xexpr (document-element (read-xml fp))))
-                              (L (get-elements arq)))
-                         (first L))))))))
-
 (define (prova->xexpr fp)
   (with-input-from-file fp
     (lambda () (xml->xexpr (document-element (read-xml (current-input-port)))))
