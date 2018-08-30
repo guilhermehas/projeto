c2a8d3d2dcffe997cf8bcb110dd6c88d08221318
joaoc <joao.carabetta@gmail.com>
2018-08-30 13:16:57 -0300

    start tfidf implementation

--- src/tfidf/tfidf.rkt
+++ src/tfidf/tfidf.rkt
@@ -1,15 +1,39 @@
 #lang racket
 
+(require data-science)
+
+(define text-e "guilherme foi Para a praia e foi\n para\t\n a fazenda. Eu não fui para praia nem para a fazenda")
+(define text-f "joao foi para praia, rademacker para a fazenda mas não gostou")
+
+;; Set[String]
+(define stopwords  
+        (list->set 
+            (map (lambda (x) (string-trim x))
+                (file->lines "src/tfidf/stopwords.txt"))))
+
+
 (define (treat-strings string)
-    (string-downcase (string-normalize-spaces string)))
+    (filter-not (λ (e) (set-member? stopwords e))
+    (string-split 
+        (remove-punctuation
+        (string-downcase 
+        (string-normalize-spaces string))))))
 
 ;; string --> (list of (list of 'str' float))
-(define (tf string)
-    (let* ((list-string (string-split (treat-strings string)))
+(define (string->tokens string)
+    (let* ((list-string (treat-strings string))
             (total (length list-string)))
-        (map (lambda (x) (list (first x) (/ (second x) total)))
-            (map (lambda (x) (list (first x) (length x)))
-            (group-by (lambda (x) x) list-string))))
-)
+                (map (lambda (x) (list (first x) (length x)))
+                (group-by (lambda (x) x) list-string))))
 
-(tf "guilherme foi Para a praia e foi\n para\t\n a fazenda")
+;;; (list of strings) --> list of (list of strings) and array of floats
+(define (tf-idf corpus)
+    (map string->tokens corpus))
+
+(tf-idf (list text-e text-f))
+
+;;; ;;; Convert each document to a list of term frequencies, passing both
+;;; ;;; to dtm
+;;; (dtm (string->tokens text-e)
+;;;      (string->tokens text-f))
+
