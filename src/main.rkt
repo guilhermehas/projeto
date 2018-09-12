e59a4decd2a2cb9d25def2c47c77334a77d1d6d4
Hugo Barreto <hugobarreto94@gmail.com>
2018-09-12 13:45:39 -0300

    Adicionado módulo main e command line parsing

--- src/main.rkt
+++ src/main.rkt
@@ -1,66 +1,93 @@
 #lang racket/base
 
 ; calling (P) and (L) results in the 2010-01 test and the 8906 law, respectively.
+(module+ main
+  (require
+    racket/cmdline
+    racket/string
+    racket/list
+    txexpr
+    math/array
+    xml
+    xml/path
+    "tfidf/read_exam.rkt"
+    "tfidf/read_law.rkt"
+    "tfidf/tfidf.rkt"
+    "graph/graph.rkt"
+    )
 
-(require
- racket/string
- racket/list
- txexpr
- math/array
- xml
- xml/path
- "tfidf/read_exam.rkt"
- "tfidf/read_law.rkt"
- "tfidf/tfidf.rkt"
- "graph/graph.rkt"
- )
+  (define laws-path (make-parameter "data/raw/leis/"))
+  (define exams-path (make-parameter "data/raw/provas/"))
+  
+  (define cmd-line
+    (command-line
+     #:program "projeto-eda"
+     #:usage-help 
+     "Solve OAB exams through tf-idf"
+     "---------------------"
+     #:once-each
+     [("--lp" "--laws-path") LAWSPATH
+                             "Setting path to dir where the laws are archived"
+                             (laws-path LAWSPATH)]
+     [("--ep" "--exams-path") EXAMSPATH
+                             "Setting path to dir where the laws are archived"
+                             (exams-path EXAMSPATH)]
+ 
+     #:args (exam)
 
-(define exam (read-exam "data/raw/provas/2010-01.xml"))
+     (string-append (exams-path) exam)))
 
-(define laws (read-law "data/raw/leis/"))
+  ;(define exam (read-exam "data/raw/provas/2010-01.xml"))
 
-(define (prepare-one-exam exam)
-  (define questions-answers '())
-  (for ((question exam))
-    (set! questions-answers (cons (cons (question-statement question)
-                                    (map (lambda (x) (item-statement x)) 
-                                      (question-items question)))
-                                   questions-answers))
-  )
-  (reverse questions-answers)
-)
+  ;(define laws (read-law "data/raw/leis/"))
 
-(define (prepare-laws laws)
-  (map (lambda (x) (article-statement x)) laws)
-)
+  (define laws (read-law laws-path))
+  (define exam (read-exam cmd-line))
 
-(define (array->listofvectors array)
-  (map list->vector (array->list* array))
-)
+  (define (prepare-one-exam exam)
+    (define questions-answers '())
+    (for ((question exam))
+      (set! questions-answers (cons (cons (question-statement question)
+                                          (map (lambda (x) (item-statement x)) 
+                                               (question-items question)))
+                                    questions-answers)))
+    (reverse questions-answers))
 
-(define (apply-tfidf question list-laws)
-  (define tfidf-matrix (second (tf-idf (append question list-laws))))
-  (define question-vector (array->vector (array-slice-ref tfidf-matrix 
-                          (list (list 0) (::)))))
-  (define answers-vector  
-          (array->listofvectors 
-            (array-slice-ref tfidf-matrix (list (:: 1 5 1) (::)))))
-  (define laws-vector  
-          (array->listofvectors 
-            (array-slice-ref tfidf-matrix (list (:: 6 #f 1) (::)))))
-  (values question-vector answers-vector laws-vector)
-)
+  (define (prepare-laws laws)
+    (map (lambda (x) (article-statement x)) laws))
+    
 
+  (define (array->listofvectors array)
+    (map list->vector (array->list* array)))
+    
 
-(define list-questions (prepare-one-exam exam))
-(define list-laws (prepare-laws laws))
-
-(define (apply-model tfidf-func question list-laws)
-  (let-values (((question answers laws) (tfidf-func question list-laws)))
-    (get-best-law question laws answers))
-)
+  (define (apply-tfidf question list-laws)
+    (define tfidf-matrix (second (tf-idf (append question list-laws))))
+    (define question-vector (array->vector (array-slice-ref tfidf-matrix 
+                                                            (list (list 0) (::)))))
+    (define answers-vector  
+      (array->listofvectors 
+       (array-slice-ref tfidf-matrix (list (:: 1 5 1) (::)))))
+    (define laws-vector  
+      (array->listofvectors 
+       (array-slice-ref tfidf-matrix (list (:: 6 #f 1) (::)))))
+    (values question-vector answers-vector laws-vector))
+    
 
 
-;;; (apply-tfidf (first list-questions) list-laws)
-(apply-model apply-tfidf (second list-questions) list-laws)
+  (define list-questions (prepare-one-exam exam))
+  (define list-laws (prepare-laws laws))
 
+  (define (apply-model tfidf-func question list-laws)
+    (let-values (((question answers laws) (tfidf-func question list-laws)))
+      (get-best-law question laws answers)))
+    
+
+
+  ;;; (apply-tfidf (first list-questions) list-laws)
+  (apply-model apply-tfidf (second list-questions) list-laws))
+
+  
+
+
+  
